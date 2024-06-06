d91e773adf62aefd9b0c3095ad27d4996e740501
Edo <muhammadridho5654@gmail.com>
2024-06-06 23:44:03 +0700

    different cookie options

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ app.use(
     cookie: {
       maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
       httpOnly: true, // prevent client side js from reading the cookie (XSS attack)
-      sameSite: "lax", // csrf
+      sameSite: "none",
       secure: false,
     },
 
