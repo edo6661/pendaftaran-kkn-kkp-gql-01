98e8737bc3cc19ac613b5d1d2b0bb77e6766eea4
Edo <muhammadridho5654@gmail.com>
2024-07-25 12:05:06 +0700

    parse tanggalMulai dan selesai

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,2 @@
-export const parseDate = (dateStr?: string): Date | null => {
-  const date = dateStr ? new Date(dateStr) : null;
-
-  return isNaN(date?.getTime()!) ? null : date;
-};
+export const parseDate = (dateStr?: string): Date | null =>
+  dateStr ? new Date(dateStr) : null;
